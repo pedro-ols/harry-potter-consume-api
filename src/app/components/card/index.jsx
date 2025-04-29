3f21b0bcbeec64ae9dbd71d071a66f512caca35e
Pedro de Oliveira Santos <pedrosantos.tds2.senai@gmail.com>
2025-04-28 21:20:00 -0300

    feat: add scrollbar style

--- src/app/components/card/index.jsx
+++ src/app/components/card/index.jsx
@@ -4,10 +4,15 @@ const Card = ({ character }) => {
     return (
         <div className={styles.card}>
             <div className={styles.characterImageContainer}>
-                <img src={character.image || "#"} alt="Imagem do personagem" className={styles.cardImage}/>
+                <img src={character.image || "/images/foto-perfil-vazia.png"} alt="Imagem do personagem" className={styles.cardImage}/>
             </div>
             <div className={styles.cardTitle}>
                 <h1 className={styles.character}>{character.name}</h1>
+                <h2 className={styles.characterText}>👶 {character.dateOfBirth || "none"}</h2>
+                <h2 className={styles.characterText}>🏠 {character.house || "none"}</h2>
+                <h2 className={styles.characterText}>🎭 {character.actor || "none"}</h2>
+                <h2 className={styles.characterText}>🧙‍♂️ {character.species || "none"}</h2>
+                <h2 className={styles.characterText}>🪄 {character.patronus || "none"}</h2>
             </div>
         </div>
     )
